d219cefb95a9b61a10f28a6f9d15150936cdb56d
LeeHsss <lii0730@icloud.com>
2022-01-26 00:39:38 +0900

    DetailViewController 데이터 파싱 전 작업까지 완료

---
+++ CreditCardRecommend/CardListVIewController.swift
@@ -0,0 +1,56 @@
+//
+//  CardListVIewController.swift
+//  CreditCardRecommend
+//
+//  Created by LeeHsss on 2022/01/25.
+//
+
+import Foundation
+import UIKit
+import Kingfisher //이미지 url을 전달해서 이미지 표시
+
+class CardListViewController: UITableViewController {
+    
+    var creditCardList: [CreditCard] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //UITableView Cell Register
+        
+        let nibName = UINib(nibName: "CardListCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.creditCardList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell else { return UITableViewCell()}
+        
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)위"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
+        
+        let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
+        cell.cardImageView.kf.setImage(with: imageURL)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80 
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 상세화면 전달
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        guard let detailViewController = storyboard.instantiateViewController(withIdentifier: "CardDetailViewController") as? CardDetailViewController else { return }
+        
+        detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail
+        self.show(detailViewController, sender: nil)
+    }
+}
+
+
---
+++ CreditCardRecommend/CreditCard.swift
@@ -0,0 +1,28 @@
+//
+//  CreditCard.swift
+//  CreditCardRecommend
+//
+//  Created by LeeHsss on 2022/01/25.
+//
+
+import Foundation
+
+struct CreditCard: Codable {
+    let id: Int
+    let rank: Int
+    let name: String
+    let cardImageURL: String
+    let promotionDetail: PromotionDetail
+    let isSelected: Bool?
+}
+
+
+struct PromotionDetail: Codable {
+    let companyName: String
+    let amount: Int
+    let period: String
+    let benefitDate: String
+    let benefitDetail: String
+    let benefitCondition: String
+    let condition: String
+}
